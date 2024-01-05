62ef19f027c54f63c6e8f831cee3483802128232
Alexander Shchirov <shchirov1998@mail.ru>
2024-01-05 13:36:02 +0300

    commit-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./App.css";
+import { getJSDocReturnType } from "typescript";
 
-function App() {
-    return <div className="App">Hello, samurai! Let's go!</div>;
-}
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Technologies />
+        </>
+    );
+};
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+};
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">News</a>
+            <a href="#s">Messages</a>
+        </div>
+    );
+};
 
 export default App;

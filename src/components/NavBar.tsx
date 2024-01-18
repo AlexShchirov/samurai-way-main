7590332a15806efd8516343fd83c87f1906839ef
Alexander Shchirov <shchirov1998@mail.ru>
2024-01-18 16:33:51 +0300

    modules creating

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,22 @@
 import React from "react";
+import s from './NavBar.module.css' ;
 
 export const NavBar = () => {
     return (
-        <nav className="nav">
-                <div>
+        <nav className={s.nav}>
+                <div className={s.item}>
                     <a href="1">Profile</a>
                 </div>
-                <div>
+                <div className={s.item}>
                     <a href="2">Messages</a>
                 </div>
-                <div>
+                <div className={s.item}>
                     <a href="3">News</a>
                 </div>
-                <div>
+                <div className={`${s.item} ${s.active}`}>
                     <a href="4">Mesic</a>
                 </div>
-                <div>
+                <div className={s.item}>
                     <a href="5">Settings</a>
                 </div>
             </nav>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css' ;
 
 export const Profile = () => {
     return (
-        <div className="profile">
+        <div className={s.profile}>
         <div>
             <img
                 className="wideImg"
@@ -15,9 +16,9 @@ export const Profile = () => {
             my posts
             <div>new post</div>
         </div>
-        <div>
-            <div>post 1</div>
-            <div>post 2</div>
+        <div className={s.posts}>
+            <div className={s.item}>post 1</div>
+            <div className={s.item}>post 2</div>
         </div>
     </div>
     );
